e5dd74777f61fe4b1ca90e1933a1b9091b7acab3
Anurup Reddy Koduru <ark@Anurups-MacBook-Air.local>
2025-09-01 01:31:30 -0500

    Made some changes in admin.py file and added a test data

--- weather/admin.py
+++ weather/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Region, WeatherParameter, WeatherData
 
-# Register your models here.
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ['name', 'code', 'description', 'created_at']
+    search_fields = ['name', 'code']
+
+
+@admin.register(WeatherParameter)
+class WeatherParameterAdmin(admin.ModelAdmin):
+    list_display = ['name', 'code', 'unit', 'description']
+    search_fields = ['name', 'code']
+
+
+@admin.register(WeatherData)
+class WeatherDataAdmin(admin.ModelAdmin):
+    list_display = ['region', 'parameter', 'year',
+                    'month', 'value', 'is_provisional']
+    list_filter = ['region', 'parameter', 'year', 'is_provisional']
+    search_fields = ['region__name', 'parameter__name']
